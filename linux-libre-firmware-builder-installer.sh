b6b1a81a746782f832ac0c70379f4bcd9a94a476
shelldweller <shelldweller@beauxbead.com>
2023-08-25 01:54:35 -0500

    fine-tune prefix path

--- linux-libre-firmware-builder-installer.sh
+++ linux-libre-firmware-builder-installer.sh
@@ -62,7 +62,7 @@ sed -i 's/( cd linux-firmware/(cd linux-libre-firmware/' kernel-firmware-gnu.Sla
 sed -i 's/rm -rf linux-firmware/rm -rf linux-libre-firmware/' kernel-firmware-gnu.SlackBuild
 sed -i 's/kernel-firmware-${DATE}/kernel-firmware-gnu-${DATE}/' kernel-firmware-gnu.SlackBuild
 sed -i '/echo "Running make DESTDIR=$PKG ${INSTALLTARGET}..."/a   make all' kernel-firmware-gnu.SlackBuild
-sed -i 's/make DESTDIR=$PKG/make prefix=$PKG/lib/firmware/' kernel-firmware-gnu.SlackBuild
+sed -i 's/make DESTDIR=$PKG/make prefix=$PKG/lib/firmware' kernel-firmware-gnu.SlackBuild
 
 # Skip building ARM-based firmware, cross-compiler needed
 sed -i "/# Better determine these the same way as above./a sed -i 's/all: aica ath9k_htc atusb av7110 carl9170fw cis dsp56k isci keyspan_pda openfwwf usbdux/all: ath9k_htc carl9170fw cis dsp56k ihex2fw isci keyspan_pda openfwwf usbdux/' linux-libre-firmware/Makefile" kernel-firmware-gnu.SlackBuild
